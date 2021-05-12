761732de6a3eaed5757719789eaf7fcb27916a98
Kevin Tan <kevin.tan@liferay.com>
2021-05-12 16:09:01 -0700

    Add sass processing

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,8 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+	stories: ["../stories/**/*.mdx", "../stories/**/*.@(js|jsx|ts|tsx)"],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/preset-scss",
+	],
+};
