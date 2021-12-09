7a747ffc6d3ebbefa970b52d4567334b9e5c7472
Kevin Tan <kevin.tan@liferay.com>
2021-12-08 16:47:30 -0800

    Add script to create theme image path

--- scripts/copy-static-files.js
+++ scripts/copy-static-files.js
@@ -4,29 +4,53 @@ const dotenvExpand = require("dotenv-expand");
 const myEnv = dotenv.config();
 dotenvExpand(myEnv);
 
-const fs = require("fs");
+const fs = require("fs-extra");
 
-function callback(error, filePath) {
-	if (error) throw error;
+/**
+ * For fixing the error:
+ * Module not found: Error: Can't resolve './@theme_image_path@/states/success_state.gif' in
+ * '/Users/ktan/Developer/liferay-portal-master/portal/modules/apps/frontend-theme/frontend-theme-admin/build/css'
+ *
+ * This creates a new folder "@theme_image_path@" in portal build folder.
+ */
+function createThemeImagePath() {
+	const source = `${process.env.PORTAL_PATH}/modules/apps/frontend-theme/frontend-theme-admin/build/images`;
+	const destination = `${process.env.PORTAL_PATH}/modules/apps/frontend-theme/frontend-theme-admin/build/css/@theme_image_path@`;
 
-	console.log(
-		`${filePath} was successfully copied to ${process.env.STATIC_PATH}`
-	);
+	if (!fs.existsSync(destination)) {
+		fs.mkdirSync(destination);
+	}
+
+	fs.copy(source, destination, function (error) {
+		if (error) {
+			console.log("An error occurred while copying the folder.");
+
+			return console.error(error);
+		}
+
+		console.log(`✅ Successfully copied:`);
+		console.log(`Source: ${source}`);
+		console.log(`Destination: ${destination}`);
+	});
 }
 
 /**
  * Copies static files from portal.
  */
 function copyStaticFiles() {
-	fs.copyFile(
-		`${process.env.PORTAL_PATH}/modules/apps/frontend-theme/frontend-theme-unstyled/src/main/resources/META-INF/resources/_unstyled/images/clay/icons.svg`,
-		`.${process.env.STATIC_PATH}/icons.svg`,
-		(error) =>
-			callback(
-				error,
-				`${process.env.PORTAL_PATH}/modules/apps/frontend-theme/frontend-theme-unstyled/src/main/resources/META-INF/resources/_unstyled/images/clay/icons.svg`
-			)
-	);
+	const source = `${process.env.PORTAL_PATH}/modules/apps/frontend-theme/frontend-theme-unstyled/src/main/resources/META-INF/resources/_unstyled/images/clay/icons.svg`;
+	const destination = `.${process.env.STATIC_PATH}/icons.svg`;
+
+	fs.copyFile(source, destination, (error) => {
+		if (error) {
+			console.error(error);
+		}
+
+		console.log(`✅ Successfully copied:`);
+		console.log(`Source: ${source}`);
+		console.log(`Destination: ${destination}`);
+	});
 }
 
 copyStaticFiles();
+createThemeImagePath();
