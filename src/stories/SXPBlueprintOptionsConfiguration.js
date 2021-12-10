f44d920b710b4d6872076f6b333998f84c66334f
Kevin Tan <kevin.tan@liferay.com>
2021-12-10 01:10:03 -0800

    Add SXPBlueprintOptionsConfiguration story

---
+++ src/stories/SXPBlueprintOptionsConfiguration.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Configuration from "src/main/resources/META-INF/resources/sxp_blueprint_options/js/configuration/index";
+
+export default {
+	title: "Components/SXPBlueprintOptionsConfiguration",
+	component: Configuration,
+};
+
+const Template = (args) => <Configuration />;
+
+export const Default = Template.bind({});
