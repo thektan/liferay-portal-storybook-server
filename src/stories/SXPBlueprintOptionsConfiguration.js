ee5f5a90d56946e81eaf3a3a994d046bce653e52
Kevin Tan <kevin.tan@liferay.com>
2021-12-15 15:21:56 -0800

    Temporarily disable SXPBlueprintOptionsConfiguration so storybook will start

--- src/stories/SXPBlueprintOptionsConfiguration.js
+++ src/stories/SXPBlueprintOptionsConfiguration.js
@@ -1,12 +1,12 @@
-import React from "react";
+// import React from "react";
 
-import Configuration from "src/main/resources/META-INF/resources/sxp_blueprint_options/js/configuration/index";
+// import Configuration from "src/main/resources/META-INF/resources/sxp_blueprint_options/js/configuration/index";
 
-export default {
-	title: "Components/SXPBlueprintOptionsConfiguration",
-	component: Configuration,
-};
+// export default {
+// 	title: "Components/SXPBlueprintOptionsConfiguration",
+// 	component: Configuration,
+// };
 
-const Template = (args) => <Configuration />;
+// const Template = (args) => <Configuration />;
 
-export const Default = Template.bind({});
+// export const Default = Template.bind({});
