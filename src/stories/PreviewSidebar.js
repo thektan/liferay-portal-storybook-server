8dfc61ef12d1a297091a4033a829a92bba5585a9
Olivia Yu <olivia.yu@liferay.com>
2021-12-17 09:08:27 -0800

    Update PreviewSidebar for new response

--- src/stories/PreviewSidebar.js
+++ src/stories/PreviewSidebar.js
@@ -17,9 +17,12 @@ const Template = (args) => <PreviewSidebar {...args} />;
 
 export const Default = Template.bind({});
 
+const SEARCH_RESULTS = mockSearchResults();
+
 Default.args = {
 	loading: false,
-	results: mockSearchResults(),
+	responseString: SEARCH_RESULTS.responseString,
+	totalHits: SEARCH_RESULTS.totalHits,
 	visible: true,
 };
 
@@ -27,116 +30,110 @@ export const Empty = Template.bind({});
 
 Empty.args = {
 	...Default.args,
-	results: mockSearchResults(0),
+	responseString: "",
+	totalHits: 0,
 };
 
 export const Errors = Template.bind({});
 
 Errors.args = {
 	...Default.args,
-	results: {
-		errors: [
-			{
-				className:
-					"com.liferay.portal.search.tuning.blueprints.engine.internal.searchrequest.QuerySearchRequestBodyContributor",
-				elementId: "queryElement-1",
-				localizationKey: "core.error.unknown-occur-value",
-				msg:
-					"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.Occur.FILTERED",
-				rootObject: {
-					context: "query",
-					occur: "filtered",
-					query: {
-						wrapper: {
-							query: {
-								range: {
-									createDate: {
-										from: "",
-										include_lower: true,
-										include_upper: true,
-										to: "",
-									},
+	errors: [
+		{
+			className:
+				"com.liferay.portal.search.tuning.blueprints.engine.internal.searchrequest.QuerySearchRequestBodyContributor",
+			elementId: "queryElement-1",
+			localizationKey: "core.error.unknown-occur-value",
+			msg:
+				"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.Occur.FILTERED",
+			rootObject: {
+				context: "query",
+				occur: "filtered",
+				query: {
+					wrapper: {
+						query: {
+							range: {
+								createDate: {
+									from: "",
+									include_lower: true,
+									include_upper: true,
+									to: "",
 								},
 							},
 						},
 					},
 				},
-				rootProperty: null,
-				rootValue: "FILTERED",
-				severity: "ERROR",
-				throwable: {
-					cause: null,
-					localizedMessage:
-						"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.Occur.FILTERED",
-					message:
-						"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.Occur.FILTERED",
-				},
 			},
-			{
-				className:
-					"com.liferay.portal.search.tuning.blueprints.engine.internal.searchrequest.QuerySearchRequestBodyContributor",
-				localizationKey: "core.error.unknown-clause-context",
-				msg:
+			rootProperty: null,
+			rootValue: "FILTERED",
+			severity: "ERROR",
+			throwable: {
+				cause: null,
+				localizedMessage:
+					"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.Occur.FILTERED",
+				message:
+					"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.Occur.FILTERED",
+			},
+		},
+		{
+			className:
+				"com.liferay.portal.search.tuning.blueprints.engine.internal.searchrequest.QuerySearchRequestBodyContributor",
+			localizationKey: "core.error.unknown-clause-context",
+			msg:
+				"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.ClauseContext.QUERYS",
+			rootObject: {
+				context: "querys",
+				occur: "filter",
+				query: {},
+			},
+			rootProperty: null,
+			rootValue: "QUERYS",
+			severity: "ERROR",
+			throwable: {
+				cause: null,
+				localizedMessage:
 					"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.ClauseContext.QUERYS",
-				rootObject: {
-					context: "querys",
-					occur: "filter",
-					query: {},
-				},
-				rootProperty: null,
-				rootValue: "QUERYS",
-				severity: "ERROR",
-				throwable: {
-					cause: null,
-					localizedMessage:
-						"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.ClauseContext.QUERYS",
-					message:
-						"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.ClauseContext.QUERYS",
-				},
+				message:
+					"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.ClauseContext.QUERYS",
 			},
-		],
-	},
+		},
+	],
 };
 
 export const Warning = Template.bind({});
 
 Warning.args = {
 	...Default.args,
-	results: {
-		...mockSearchResults(),
-		warnings: [{msg: "The configuration has missing or invalid values."}],
-	},
+	warnings: [{msg: "The configuration has missing or invalid values."}],
 };
 
 export const WarningsAndErrors = Template.bind({});
 
 WarningsAndErrors.args = {
 	...Default.args,
-	results: {
-		warnings: [{msg: "The configuration has missing or invalid values."}],
-		errors: [
-			{
-				className:
-					"com.liferay.portal.search.tuning.blueprints.engine.internal.searchrequest.QuerySearchRequestBodyContributor",
-				localizationKey: "core.error.unknown-clause-context",
-				msg:
-					"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.ClauseContext.QUERYS",
-				rootObject: {
-					context: "querys",
-					occur: "filter",
-					query: {},
-				},
-				rootProperty: null,
-				rootValue: "QUERYS",
-				severity: "ERROR",
-				throwable: {
-					cause: null,
-					localizedMessage:
-						"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.ClauseContext.QUERYS",
-					message:
-						"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.ClauseContext.QUERYS",
-				},
+	warnings: [{msg: "The configuration has missing or invalid values."}],
+	errors: [
+		{
+			className:
+				"com.liferay.portal.search.tuning.blueprints.engine.internal.searchrequest.QuerySearchRequestBodyContributor",
+			localizationKey: "core.error.unknown-clause-context",
+			msg:
+				"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.ClauseContext.QUERYS",
+			rootObject: {
+				context: "querys",
+				occur: "filter",
+				query: {},
 			},
-		],
-	},
+			rootProperty: null,
+			rootValue: "QUERYS",
+			severity: "ERROR",
+			throwable: {
+				cause: null,
+				localizedMessage:
+					"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.ClauseContext.QUERYS",
+				message:
+					"No enum constant com.liferay.portal.search.tuning.blueprints.constants.json.values.ClauseContext.QUERYS",
+			},
+		},
+	],
 };
