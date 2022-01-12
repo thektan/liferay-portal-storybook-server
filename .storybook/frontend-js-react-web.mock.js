ae29d8e8e943200e947407f2f690ddaf2629fea8
Kevin Tan <kevin.tan@liferay.com>
2022-01-12 10:30:27 -0800

    Implement useIsMounted

--- .storybook/frontend-js-react-web.mock.js
+++ .storybook/frontend-js-react-web.mock.js
@@ -6,6 +6,23 @@
 const React = require("react");
 
 module.exports = {
+	// See: https://github.com/liferay/liferay-portal/blob/18f9aefe633dbb4364b7ebc82a17b9e124d764ac/modules/apps/frontend-js/frontend-js-react-web/src/main/resources/META-INF/resources/js/hooks/useIsMounted.ts
+
+	useIsMounted: () => {
+		const mountedRef = React.useRef(false);
+		const isMounted = React.useCallback(() => mountedRef.current, []);
+
+		React.useLayoutEffect(() => {
+			mountedRef.current = true;
+
+			return () => {
+				mountedRef.current = false;
+			};
+		}, []);
+
+		return isMounted;
+	},
+
 	// See: https://github.com/liferay/liferay-portal/blob/afcb92c2e1/modules/apps/frontend-js/frontend-js-react-web/src/main/resources/META-INF/resources/js/hooks/usePrevious.es.js
 
 	usePrevious: (value) => {
@@ -17,6 +34,4 @@ module.exports = {
 
 		return ref.current;
 	},
-
-	useIsMounted: () => () => {},
 };
