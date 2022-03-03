d93a4a1dd580eb9bb38bb02f25562a19f16dbfca
Olivia Yu <olivia.yu@liferay.com>
2022-03-03 15:55:19 -0800

    Enable blueprint options configuration

--- src/stories/SXPBlueprintOptionsConfiguration.js
+++ src/stories/SXPBlueprintOptionsConfiguration.js
@@ -1,12 +1,14 @@
-// import React from "react";
+import React from "react";
 
-// import Configuration from "src/main/resources/META-INF/resources/sxp_blueprint_options/js/configuration/index";
+import Configuration from "src/main/resources/META-INF/resources/sxp_blueprint_options/js/configuration/index";
 
-// export default {
-// 	title: "Components/SXPBlueprintOptionsConfiguration",
-// 	component: Configuration,
-// };
+import "src/main/resources/META-INF/resources/sxp_blueprint_options/css/main.scss";
 
-// const Template = (args) => <Configuration />;
+export default {
+	title: "Components/SXPBlueprintOptionsConfiguration",
+	component: Configuration,
+};
 
-// export const Default = Template.bind({});
+const Template = (args) => <Configuration />;
+
+export const Default = Template.bind({});
