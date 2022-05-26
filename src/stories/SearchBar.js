d5534a0b7b85b287f68cd059dce5a5529d491947
Kevin Tan <kevin.tan@liferay.com>
2022-05-26 14:27:07 -0700

    Add Search Bar multiple contributor stories

--- src/stories/SearchBar.js
+++ src/stories/SearchBar.js
@@ -16,3 +16,24 @@ const Template = (args) => (
 export const Default = Template.bind({});
 
 Default.args = {};
+
+export const OneContributor = Template.bind({});
+
+OneContributor.args = {
+	suggestionsURL:
+		"https://run.mocky.io/v3/314e74ec-0aa5-4986-8c33-9e267c3e354c",
+};
+
+export const TwoContributors = Template.bind({});
+
+TwoContributors.args = {
+	suggestionsURL:
+		"https://run.mocky.io/v3/72184277-21e0-4d9a-9892-cd87d42f78c7",
+};
+
+export const FiveContributors = Template.bind({});
+
+FiveContributors.args = {
+	suggestionsURL:
+		"https://run.mocky.io/v3/67e7e328-2b0f-4780-89e8-dd5bae55b2cd",
+};
