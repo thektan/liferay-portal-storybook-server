c65b0e58c2d9007b7c31f852bdde4900bb79ec32
Kevin Tan <kevin.tan@liferay.com>
2022-06-01 13:52:00 -0700

    Add search bar with scope story

--- src/stories/SearchBar.js
+++ src/stories/SearchBar.js
@@ -37,3 +37,11 @@ FiveContributors.args = {
 	suggestionsURL:
 		"https://run.mocky.io/v3/67e7e328-2b0f-4780-89e8-dd5bae55b2cd",
 };
+
+export const WithScope = Template.bind({});
+
+WithScope.args = {
+	letUserChooseScope: true,
+	suggestionsURL:
+		"https://run.mocky.io/v3/314e74ec-0aa5-4986-8c33-9e267c3e354c",
+};
