eeddf64b4aec88f89a5dfc027fe8d104364aace8
Olivia Yu <olivia.yu@liferay.com>
2022-06-23 08:44:18 -0700

    Add a CodeMirrorEditor story

---
+++ src/stories/CodeMirrorEditor.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import CodeMirrorEditor from "src/main/resources/META-INF/resources/sxp_blueprint_admin/js/shared/CodeMirrorEditor";
+import sxpQueryElementSchema from "src/main/resources/META-INF/resources/sxp_blueprint_admin/schemas/sxp-query-element.schema.json";
+import {QUERY_SXP_ELEMENTS} from "test/js/mocks/data.js";
+import {ThemeContextDecorator} from "../decorators";
+
+export default {
+	title: "Components/CodeMirrorEditor",
+	component: CodeMirrorEditor,
+	decorators: [ThemeContextDecorator],
+};
+
+const Template = (args) => (
+	<CodeMirrorEditor
+		autocompleteSchema={sxpQueryElementSchema}
+		onChange={() => {}}
+		value={JSON.stringify(QUERY_SXP_ELEMENTS[0], null, 2)}
+	/>
+);
+
+export const Default = Template.bind({});
