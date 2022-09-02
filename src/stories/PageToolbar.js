41daf81efa11975195a8a34f6856f107bd789b0b
Olivia Yu <olivia.yu@liferay.com>
2022-09-02 10:21:10 -0700

    Update PageToolbar for localized title/description

--- src/stories/PageToolbar.js
+++ src/stories/PageToolbar.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 import PageToolbar from "src/main/resources/META-INF/resources/sxp_blueprint_admin/js/shared/PageToolbar";
-import {EditBlueprintDecorator} from "../decorators";
+import {EditBlueprintDecorator, ThemeContextDecorator} from "../decorators";
 
 export default {
 	title: "Components/PageToolbar",
-	decorators: [EditBlueprintDecorator],
+	decorators: [EditBlueprintDecorator, ThemeContextDecorator],
 	parameters: {
 		layout: "fullscreen",
 	},
@@ -17,7 +17,14 @@ const Template = (args) => <PageToolbar {...args} />;
 export const Default = Template.bind({});
 
 Default.args = {
-	title: "Test Title",
+	title: {
+		"en-US": "Test Title",
+		"es-ES": "Test Titulo",
+	},
+	description: {
+		"en-US": "Test Description",
+		"es-ES": "Test Descripcion",
+	},
 	onCancel: "#",
 	tab: "query-builder",
 	tabs: {
