953da4b42ae95a4df69e4450cf2066d0b8f190a4
Kevin Tan <kevin.tan@liferay.com>
2022-10-27 11:46:51 -0700

    Create TestConnectionButton.js

---
+++ src/stories/TestConnectionButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import TestConnectionButton from "src/main/resources/META-INF/resources/semantic_search/js/configuration/TestConnectionButton";
+
+export default {
+	title: "Components/TestConnectionButton",
+	component: TestConnectionButton,
+};
+
+const Template = (args) => <TestConnectionButton {...args} />;
+
+export const Default = Template.bind({});
