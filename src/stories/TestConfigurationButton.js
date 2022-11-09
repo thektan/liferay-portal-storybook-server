af2bd04622cbec708202d74b028adbe6f3476bb8
Olivia Yu <olivia.yu@liferay.com>
2022-11-09 09:26:21 -0800

    Rename TestConnection to TestConfiguration

---
+++ src/stories/TestConfigurationButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import TestConfigurationButton from "src/main/resources/META-INF/resources/semantic_search/js/configuration/TestConfigurationButton";
+
+export default {
+	title: "Components/TestConfigurationButton",
+	component: TestConfigurationButton,
+};
+
+const Template = (args) => <TestConfigurationButton {...args} />;
+
+export const Default = Template.bind({});
