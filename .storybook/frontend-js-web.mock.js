1b90362d31aff686eb79a45eb345cb0ac421859c
Olivia Yu <olivia.yu@liferay.com>
2022-11-21 14:33:03 -0800

    Mock frontend-js-web function for addParams

--- .storybook/frontend-js-web.mock.js
+++ .storybook/frontend-js-web.mock.js
@@ -7,6 +7,45 @@
 /* eslint-env browser */
 
 /**
+ * Appends given parameters to the given URL.
+ * @param {String | Object} params Parameters to be added on to the base url
+ * @param {String} baseUrl Base url
+ * @return {String} url URL constructed from base url and params
+ * @review
+ */
+function addParams(params, baseUrl) {
+	if (!params || (typeof params !== "object" && typeof params !== "string")) {
+		throw new TypeError("Parameter params must be an object or string");
+	}
+
+	if (typeof baseUrl !== "string") {
+		throw new TypeError("Parameter baseUrl must be a string");
+	}
+
+	const url = baseUrl.startsWith("/")
+		? new URL(baseUrl, location.href)
+		: new URL(baseUrl);
+
+	if (typeof params === "object") {
+		Object.entries(params).forEach(([key, value]) => {
+			url.searchParams.append(key, value);
+		});
+	} else {
+		const searchParams = new URLSearchParams(params.trim());
+
+		searchParams.forEach((value, key) => {
+			if (value) {
+				url.searchParams.append(key, value);
+			} else {
+				url.searchParams.append(key, "");
+			}
+		});
+	}
+
+	return url.toString();
+}
+
+/**
  * Debounces function execution.
  * @param {!function()} fn
  * @param {number} delay
@@ -46,6 +85,7 @@ function sub(string, data) {
 }
 
 module.exports = {
+	addParams,
 	CompatibilityEventProxy: () => {},
 	DefaultEventHandler: () => {},
 	ItemSelectorDialog: () => {},
