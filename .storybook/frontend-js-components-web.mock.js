b95560689c00c3b1bd65ad8665c8e6d3fc73fbfc
Kevin Tan <kevin.tan@liferay.com>
2022-11-29 12:54:39 -0800

    Remove console log

--- .storybook/frontend-js-components-web.mock.js
+++ .storybook/frontend-js-components-web.mock.js
@@ -8,9 +8,7 @@ const React = require("react");
 import ManagementToolbar from "../static/mock-components/src/main/resources/META-INF/resources/management_toolbar/ManagementToolbar";
 import TranslationAdminSelector from "../static/mock-components/src/main/resources/META-INF/resources/translation_manager/TranslationAdminSelector";
 import Treeview from "../static/mock-components/src/main/resources/META-INF/resources/treeview/Treeview";
-console.log(ManagementToolbar);
-console.log(TranslationAdminSelector);
-console.log(Treeview);
+
 module.exports = {
 	ManagementToolbar,
 	TranslationAdminSelector,
