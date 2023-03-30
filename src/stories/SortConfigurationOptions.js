794eafdc98d777418f94803b732cfe9c198af59b
Kevin Tan <kevin.tan@liferay.com>
2023-03-29 18:26:54 -0700

    Add learnMessages to SortConfigurationOptions

--- src/stories/SortConfigurationOptions.js
+++ src/stories/SortConfigurationOptions.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import SortConfigurationOptions from "src/main/resources/META-INF/resources/js/components/SortConfigurationOptions";
 import {SortDecorator} from "../decorators";
+import learnMessages from "../../static/portal-search-web.json";
 
 export default {
 	title: "Components/SortConfigurationOptions",
@@ -9,7 +10,9 @@ export default {
 	component: Element,
 };
 
-const Template = (args) => <SortConfigurationOptions {...args} />;
+const Template = (args) => (
+	<SortConfigurationOptions learnMessages={learnMessages} {...args} />
+);
 
 export const Default = Template.bind({});
 
