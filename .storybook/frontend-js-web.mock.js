e68e3c1077cbec3239e911c37bb0390f45eb289b
Kevin Tan <kevin.tan@liferay.com>
2023-07-12 11:09:59 -0700

    Add localStorage util

--- .storybook/frontend-js-web.mock.js
+++ .storybook/frontend-js-web.mock.js
@@ -6,6 +6,7 @@
 /* eslint-disable no-console */
 /* eslint-env browser */
 
+import LocalStorage from "../static/mock-components/src/main/resources/META-INF/resources/util/local_storage";
 import SessionStorage from "../static/mock-components/src/main/resources/META-INF/resources/util/session_storage";
 
 /**
@@ -101,6 +102,7 @@ module.exports = {
 	navigate: (url, listeners) => console.log({listeners, url}),
 	openSimpleInputModal: (config) => console.log(config),
 	openToast: (config) => console.log(config),
+	localStorage: LocalStorage,
 	sessionStorage: SessionStorage,
 	sub,
 };
