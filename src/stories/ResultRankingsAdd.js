00ee7b42dc047c2e4fc08f2295515aafddceb138
Olivia Yu <olivia.yu@liferay.com>
2023-10-03 10:15:22 -0700

    Add storybook for ResultsRankingsAdd

---
+++ src/stories/ResultRankingsAdd.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import ResultRankingsAdd from "src/main/resources/META-INF/resources/js/components/ResultRankingsAdd.es.js";
+import "src/main/resources/META-INF/resources/css/main.scss";
+
+import portalSearchTuningRankingsWeb from "../../static/learn-resources/portal-search-tuning-rankings-web.json";
+import {LearnResourcesContextDecorator} from "../decorators";
+
+export default {
+	title: "ResultRankings/ResultRankingsAdd",
+	component: ResultRankingsAdd,
+	decorators: [LearnResourcesContextDecorator],
+};
+
+const Template = (args) => (
+	<ResultRankingsAdd
+		cancelURL=""
+		learnResources={{
+			["portal-search-tuning-rankings-web"]: portalSearchTuningRankingsWeb,
+		}}
+	/>
+);
+
+export const Default = Template.bind({});
+
+Default.args = {};
