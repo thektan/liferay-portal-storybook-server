686855d36644dc1df29d05b2c6564a9d9d106251
Olivia Yu <olivia.yu@liferay.com>
2023-12-14 09:43:01 -0800

    Add limit search to these sites story

--- src/stories/EditSXPBlueprintForm.js
+++ src/stories/EditSXPBlueprintForm.js
@@ -46,3 +46,70 @@ NoElements.args = {
 	...AllElements.args,
 	initialSXPElementInstances: [],
 };
+
+export const SiteElement = Template.bind({});
+
+SiteElement.args = {
+	...AllElements.args,
+	initialSXPElementInstances: [
+		{
+			description_i18n: {
+				en_US: "limit-search-to-these-sites-description",
+			},
+			elementDefinition: {
+				category: "filter",
+				configuration: {
+					queryConfiguration: {
+						queryEntries: [
+							{
+								clauses: [
+									{
+										context: "query",
+										occur: "filter",
+										query: {
+											terms: {
+												scopeGroupIds:
+													"${configuration.scope_group_ids}",
+												scopeGroupId:
+													"${configuration.scope_group_id}",
+											},
+										},
+									},
+								],
+							},
+						],
+					},
+				},
+				icon: "filter",
+				uiConfiguration: {
+					fieldSets: [
+						{
+							fields: [
+								{
+									helpText: "group-ids-help-text",
+									label: "group-ids",
+									name: "scope_group_ids",
+									type: "multiselect",
+								},
+								{
+									helpText: "group-ids-help-text",
+									label: "group-id",
+									name: "scope_group_id",
+									type: "number",
+								},
+							],
+						},
+					],
+				},
+			},
+			externalReferenceCode: "LIMIT_SEARCH_TO_THESE_SITES",
+			title_i18n: {
+				en_US: "limit-search-to-these-sites",
+			},
+		},
+	].map((sxpElement) => ({
+		sxpElement,
+		type: 10,
+		uiConfigurationValues: getUIConfigurationValues(sxpElement),
+	})),
+};
