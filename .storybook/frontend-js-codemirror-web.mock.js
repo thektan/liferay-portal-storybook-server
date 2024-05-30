0084e4900f15ce5e25ff8efc69add9e7302c4a0d
Olivia Yu <olivia.yu@liferay.com>
2024-05-30 14:13:10 -0700

    Fix issue with codemirror mock

--- .storybook/frontend-js-codemirror-web.mock.js
+++ .storybook/frontend-js-codemirror-web.mock.js
@@ -1,30 +1,29 @@
+import "codemirror/addon/display/autorefresh";
+import "codemirror/addon/display/placeholder";
+import "codemirror/addon/edit/closebrackets";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/matchbrackets";
+import "codemirror/addon/fold/brace-fold";
+import "codemirror/addon/fold/comment-fold";
+import "codemirror/addon/fold/foldcode";
+import "codemirror/addon/fold/foldgutter";
+import "codemirror/addon/fold/indent-fold";
+import "codemirror/addon/fold/xml-fold";
+import "codemirror/addon/hint/css-hint";
+import "codemirror/addon/hint/html-hint";
+import "codemirror/addon/hint/javascript-hint";
+import "codemirror/addon/hint/show-hint";
+import "codemirror/addon/hint/xml-hint";
+import "codemirror/mode/groovy/groovy";
+import "codemirror/mode/htmlmixed/htmlmixed";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/css/css";
 
-import 'codemirror/addon/display/autorefresh';
-import 'codemirror/addon/display/placeholder';
-import 'codemirror/addon/edit/closebrackets';
-import 'codemirror/addon/edit/closetag';
-import 'codemirror/addon/edit/matchbrackets';
-import 'codemirror/addon/fold/brace-fold';
-import 'codemirror/addon/fold/comment-fold';
-import 'codemirror/addon/fold/foldcode';
-import 'codemirror/addon/fold/foldgutter';
-import 'codemirror/addon/fold/indent-fold';
-import 'codemirror/addon/fold/xml-fold';
-import 'codemirror/addon/hint/css-hint';
-import 'codemirror/addon/hint/html-hint';
-import 'codemirror/addon/hint/javascript-hint';
-import 'codemirror/addon/hint/show-hint';
-import 'codemirror/addon/hint/xml-hint';
-import 'codemirror/mode/groovy/groovy';
-import 'codemirror/mode/htmlmixed/htmlmixed';
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/xml/xml';
-import 'codemirror/mode/css/css';
+import "codemirror/addon/fold/foldgutter.css";
+import "codemirror/addon/hint/show-hint.css";
+import "codemirror/lib/codemirror.css";
 
-import 'codemirror/addon/fold/foldgutter.css';
-import 'codemirror/addon/hint/show-hint.css';
-import 'codemirror/lib/codemirror.css';
+import CodeMirror from "codemirror";
 
-import CodeMirror from 'codemirror';
-
-module.exports = CodeMirror;
+module.exports = {CodeMirror};
