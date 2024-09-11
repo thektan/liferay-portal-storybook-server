cc2e6b8b9f46fe2e2855fe6e9f8ec8b6cbdcde30
Olivia Yu <olivia.yu@liferay.com>
2024-09-11 12:32:52 -0700

    Semantic Search component moved to search-web

---
+++ src/stories/SemanticSearch.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+import SemanticSearch from "src/main/resources/META-INF/resources/js/components/semantic_search_configuration/index";
+import learnMessages from "../../static/learn-resources/portal-search-web.json";
+
+export default {
+	title: "SemanticSearch/SemanticSearch",
+	component: SemanticSearch,
+	argTypes: {onChange: {action: "changed"}, onEnter: {action: "enter"}},
+};
+
+const Template = (args) => (
+	<SemanticSearch
+		availableEmbeddingVectorDimensions={["384", "512", "768"]}
+		availableTextEmbeddingProviders={{
+			huggingFaceInferenceAPI: "Hugging Face Inference API",
+			huggingFaceInferenceEndpoint: "Hugging Face Inference Endpoint",
+			txtai: "txtai",
+		}}
+		availableTextTruncationStrategies={{
+			beginning: "Beginning",
+			middle: "Middle",
+			end: "End",
+		}}
+		initialTextEmbeddingsEnabled={true}
+		initialTextEmbeddingProviderConfigurationJSONs={[]}
+		availableLanguageDisplayNames={{
+			ar_SA: "Arabic (Saudi Arabia)",
+			ca_ES: "Catalan (Spain)",
+			zh_CN: "Chinese (China)",
+			nl_NL: "Dutch (Netherlands)",
+			en_US: "English (United States)",
+			fi_FI: "Finnish (Finland)",
+			fr_FR: "French (France)",
+			de_DE: "German (Germany)",
+			hu_HU: "Hungarian (Hungary)",
+			ja_JP: "Japanese (Japan)",
+			pt_BR: "Portuguese (Brazil)",
+			es_ES: "Spanish (Spain)",
+			sv_SE: "Swedish (Sweden)",
+		}}
+		initialTextEmbeddingCacheTimeout={0}
+		availableModelClassNames={{
+			"com.liferay.blogs.model.BlogsEntry": "Blogs Entry",
+			"com.liferay.knowledge.base.model.KBArticle":
+				"Knowledge Base Article",
+			"com.liferay.message.boards.model.MBMessage":
+				"Message Boards Message",
+			"com.liferay.journal.model.JournalArticle": "Web Content Article",
+			"com.liferay.wiki.model.WikiPage": "Wiki Page",
+		}}
+		learnMessages={learnMessages}
+		{...args}
+	/>
+);
+
+export const Default = Template.bind({});
---
+++ src/stories/TestConfigurationButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import TestConfigurationButton from "src/main/resources/META-INF/resources/js/components/semantic_search_configuration/TestConfigurationButton";
+
+export default {
+	title: "SemanticSearch/TestConfigurationButton",
+	component: TestConfigurationButton,
+};
+
+const Template = (args) => <TestConfigurationButton {...args} />;
+
+export const Default = Template.bind({});
