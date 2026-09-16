bf848d9ddab6ec155f263abac9d69f3b02f507ff
Olivia Yu <olivia.yu@liferay.com>
2026-09-16 12:02:06 -0700

    Include ColorSchemeDecorator

--- src/decorators.js
+++ src/decorators.js
@@ -38,6 +38,12 @@ export const EditElementDecorator = (Story) => (
 	</ErrorBoundary>
 );
 
+export const ColorSchemeDecorator = (colorScheme) => (Story) => {
+	document.documentElement.setAttribute("data-color-scheme", colorScheme);
+
+	return <Story />;
+};
+
 export const FeatureFlagDecorator = (featureFlag, enabled) => (Story) => {
 	window.Liferay.FeatureFlags = {
 		...window.Liferay.FeatureFlags,
